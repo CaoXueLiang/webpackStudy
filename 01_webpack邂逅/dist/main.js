0e42f090c678eeadca1af1803c4fc5c51faf7c89
xueliang cao <2357747972@qq.com>
2021-06-15 18:59:51 +0800

    webpack邂逅

---
+++ 01_webpack邂逅/dist/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";console.log(40),console.log(200)})();
---
+++ 01_webpack邂逅/src/index.js
@@ -0,0 +1,9 @@
+function sumMethod(num1, num2) {
+  return num1 + num2;
+}
+
+import { sum, mul } from "./js/math";
+
+console.log(sum(10, 30));
+
+console.log(mul(10, 20));
