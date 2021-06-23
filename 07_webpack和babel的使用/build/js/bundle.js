f46f225346091df8b08b9a9a5a08a06ccc3c5562
xueliang cao <2357747972@qq.com>
2021-06-23 14:23:48 +0800

    添加htmlWebpackPlugin，cleanWebpackPlugin等plugin

--- 07_webpack和babel的使用/build/js/bundle.js
+++ 07_webpack和babel的使用/build/js/bundle.js
@@ -1,1 +1,106 @@
-(()=>{"use strict";console.log("2020-11-22"),console.log(30),console.log(200)})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/format.js":
+/*!**************************!*\
+  !*** ./src/js/format.js ***!
+  \**************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"formateDate\": () => (/* binding */ formateDate)\n/* harmony export */ });\nfunction formateDate() {\n  return \"2020-11-22\";\n}\n\n\n//# sourceURL=webpack://07webpackbabel/./src/js/format.js?");
+
+/***/ }),
+
+/***/ "./src/js/math.js":
+/*!************************!*\
+  !*** ./src/js/math.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"sum\": () => (/* binding */ sum),\n/* harmony export */   \"mul\": () => (/* binding */ mul)\n/* harmony export */ });\nfunction sum(num1, num2) {\n  return num1 + num2;\n}\n\nfunction mul(num1, num2) {\n  return num1 * num2;\n}\n\n\n//# sourceURL=webpack://07webpackbabel/./src/js/math.js?");
+
+/***/ }),
+
+/***/ "./src/main.js":
+/*!*********************!*\
+  !*** ./src/main.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _src_js_format__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../src/js/format */ \"./src/js/format.js\");\n/* harmony import */ var _src_js_math__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../src/js/math */ \"./src/js/math.js\");\n\n\n\nconsole.log((0,_src_js_format__WEBPACK_IMPORTED_MODULE_0__.formateDate)());\nconsole.log((0,_src_js_math__WEBPACK_IMPORTED_MODULE_1__.sum)(10, 20));\nconsole.log((0,_src_js_math__WEBPACK_IMPORTED_MODULE_1__.mul)(10, 20));\n\n\n//# sourceURL=webpack://07webpackbabel/./src/main.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/main.js");
+/******/ 	
+/******/ })()
+;
--- 07_webpack和babel的使用/wk.config.js
+++ 07_webpack和babel的使用/wk.config.js
@@ -1,4 +1,6 @@
 const { resolve } = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/main.js",
@@ -6,4 +8,12 @@ module.exports = {
     filename: "js/bundle.js",
     path: resolve(__dirname, "build"),
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Babel的使用",
+      template: "./public/index.html",
+    }),
+  ],
+  mode: "development",
 };
