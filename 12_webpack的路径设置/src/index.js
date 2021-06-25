7f808b8030318a80be4335ea99c8024d20ed1e66
xueliang cao <2357747972@qq.com>
2021-06-25 14:21:51 +0800

    webpack路径publicPath设置

---
+++ 12_webpack的路径设置/src/index.js
@@ -0,0 +1,8 @@
+import { cereateDiv, createSpan } from "./js/cereatElement";
+
+const tmpDiv = cereateDiv();
+const tmpSpan = createSpan();
+document.body.appendChild(tmpDiv);
+tmpDiv.appendChild(tmpSpan);
+
+console.log("----11111---22222--3333---444444---5566665===");
---
+++ 12_webpack的路径设置/wk.config.js
@@ -0,0 +1,52 @@
+const { resolve } = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { DefinePlugin } = require("webpack");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  // watch: true, //可以在这配置监听文件的修改，也可以在package.json中设置webpack --watch
+  mode: "development",
+  devtool: "source-map",
+  entry: "./src/index.js",
+  output: {
+    filename: "js/bundle.js",
+    path: resolve(__dirname, "./build"),
+    publicPath: "/",
+  },
+
+  devServer: {
+    hot: true,
+    contentBase: "./build",
+    open: true,
+    port: "8524",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "webpack中devServe使用",
+      template: "./public/index.html",
+    }),
+    new DefinePlugin({
+      BASE_URL: '"./"',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "public",
+          globOptions: {
+            ignore: ["**/index.html", "**/DS_Store"],
+          },
+        },
+      ],
+    }),
+  ],
+};
