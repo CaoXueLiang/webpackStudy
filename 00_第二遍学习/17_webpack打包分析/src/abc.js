1fd7485f34549deb0d542cad065974386e4720d6
_誌念 <2357747972@qq.com>
2022-01-04 18:05:41 +0800

    webpck打包分析

---
+++ 00_第二遍学习/17_webpack打包分析/src/abc.js
@@ -0,0 +1,13 @@
+export function abc() {
+  return 'abc';
+}
+
+const titleDiv = document.createElement('div');
+titleDiv.className = 'title';
+document.body.appendChild(titleDiv);
+
+const h2El = document.createElement('h2');
+document.body.appendChild(h2El);
+
+const El1 = document.createElement('font_12');
+document.body.appendChild(El1);
